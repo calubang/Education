8629788e3d51f06ffe0a370480427e9868589793
calubang <acalubang@gmail.com>
2019-03-26 15:24:24 +0900

    19.03.26 langtest update
    abo StringVSBuffer

--- javaSource/langtest/src/com/kitri/lang/StringVSBuffer.java
+++ javaSource/langtest/src/com/kitri/lang/StringVSBuffer.java
@@ -3,5 +3,21 @@ package com.kitri.lang;
 public class StringVSBuffer {
 	public static void main(String[] args) {
 		
+		long startTime = System.nanoTime();
+		String str = "hello";
+		for(int i =0 ; i<5000 ; i++) {
+			str += i;
+		}
+		long endTime = System.nanoTime();
+		System.out.println(endTime - startTime);
+		
+		startTime = System.nanoTime();
+		StringBuffer sb = new StringBuffer("hello");
+		for(int i =0 ; i<5000 ; i++) {
+			sb.append(i);
+		}
+		endTime = System.nanoTime();
+		System.out.println(endTime - startTime);
+		
 	}
 }
