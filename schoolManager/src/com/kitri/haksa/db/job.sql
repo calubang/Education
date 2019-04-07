be935c5eb4fce56d2412ae5cb1203df9e95fdf65
calubang <acalubang@gmail.com>
2019-04-07 21:32:56 +0900

    19.04.07 abo update
    schoolManager sql upload

---
+++ schoolManager/src/com/kitri/haksa/db/job.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 일요일-4월-07-2019   
+--------------------------------------------------------
+REM INSERTING into KITRI.JOB
+SET DEFINE OFF;
+Insert into KITRI.JOB (KEY,KEY_NAME) values (1,'학생');
+Insert into KITRI.JOB (KEY,KEY_NAME) values (2,'교수');
+Insert into KITRI.JOB (KEY,KEY_NAME) values (3,'관리자');
---
+++ schoolManager/src/com/kitri/haksa/db/school.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 일요일-4월-07-2019   
+--------------------------------------------------------
+REM INSERTING into KITRI.SCHOOL
+SET DEFINE OFF;
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('안병욱2',32,1,'200611000');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('이재운',33,1,'200512345');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('권영찬',23,1,'201411123');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('권범준',26,2,'JAVA');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('이소담',28,3,'IT');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('여동빈',40,3,'MANAGER');
+Insert into KITRI.SCHOOL (NAME,AGE,KEY,VALUE) values ('안효인',45,2,'IT');
